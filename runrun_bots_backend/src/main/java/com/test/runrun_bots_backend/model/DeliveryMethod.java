58160ae662324670a9c2289b5e8823490d5c045a
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-03-18 14:14:32 +0800

    Update DeliveryMethod.java

--- runrun_bots_backend/src/main/java/com/test/runrun_bots_backend/model/DeliveryMethod.java
+++ runrun_bots_backend/src/main/java/com/test/runrun_bots_backend/model/DeliveryMethod.java
@@ -1,6 +1,17 @@
 package com.test.runrun_bots_backend.model;
 
 public enum DeliveryMethod {
-    ROBOT,
-    DRONE
+    ROBOT("ROBOT"),
+    DRONE("DRONE"),;
+
+    private final String description;
+
+    DeliveryMethod(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
