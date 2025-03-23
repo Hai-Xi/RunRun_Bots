6c3d52b80179d505efdd79c66310f3472db24edc
Lezheng Fang <lezheng.fang@gatech.edu>
2025-03-23 16:26:50 -0400

    Init Components (to be continued)

---
+++ Front-end/runrunbot_front_end/src/components/CreateNewOrder.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateNewOrder(props) {
+  return <div>CreateNewOrder</div>;
+}
+
+export default CreateNewOrder;
---
+++ Front-end/runrunbot_front_end/src/components/OrderManage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function OrderManage(props) {
+  return <div>OrderManage</div>;
+}
+
+export default OrderManage;
