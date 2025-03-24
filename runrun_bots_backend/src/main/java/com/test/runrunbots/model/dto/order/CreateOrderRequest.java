d4de02a23200e8435551b2dcc3438c9761873ab3
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-03-24 15:53:57 +0800

    Update CreateOrderRequest.java

--- runrun_bots_backend/src/main/java/com/test/runrunbots/model/dto/order/CreateOrderRequest.java
+++ runrun_bots_backend/src/main/java/com/test/runrunbots/model/dto/order/CreateOrderRequest.java
@@ -3,10 +3,21 @@ package com.test.runrunbots.model.dto.order;
 import lombok.Data;
 
 @Data  
-public class CreateOrderRequest {  
-    private Long userId;  
+public class CreateOrderRequest {
+    /**
+     * prder table
+     */
+    private Long userId;
     private String itemDescription;  
     private String pickupLocation;  
     private String deliveryLocation;  
-    private String deliveryMethod; // ROBOT or DRONE  
+    private String deliveryMethod; // ROBOT or DRONE
+
+    /**
+     * payment table
+     */
+    private Double totalAmount;   // Total order amount
+    private String paymentMethod; // Payment method, e.g., Credit Card
+    private String paymentStatus; // Payment status, e.g., PENDING
+
 }  
