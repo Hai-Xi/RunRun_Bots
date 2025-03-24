cccb04d2aef13f4497e6cbfa6ba9df06cb13045b
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-03-24 17:09:06 +0800

    Update Order.java

--- runrun_bots_backend/src/main/java/com/test/runrunbots/model/Order.java
+++ runrun_bots_backend/src/main/java/com/test/runrunbots/model/Order.java
@@ -54,7 +54,13 @@ public class Order {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
-    @OneToMany(mappedBy = "order")  
+    /**
+     *
+     * mappedBy: 被维护端使用，表示由另一端 (即 order )的哪个字段来维护这段关系。这里 Payment 的 order 是拥有端。
+     * cascade = CascadeType.ALL: 表示在操作 Order 实体时，相关联的 Payment 实体也会被自动操作。例如创建 Order 时，会自动保存 Payment。
+     *
+     */
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<Payment> payments;
 
     @OneToMany(mappedBy = "order")  
