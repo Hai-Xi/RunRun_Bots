aa051c2c081e47403bd65074f1f15ca4aaf39358
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-03-25 15:41:09 +0800

    perf(model): unifed response handling

---
+++ runrun_bots_backend/src/main/java/com/test/runrunbots/model/dto/user/UserRegistrationResponse.java
@@ -0,0 +1,13 @@
+package com.test.runrunbots.model.dto.user;
+
+import com.test.runrunbots.model.UserRole;
+import lombok.Data;
+
+@Data  
+public class UserRegistrationResponse {
+    private String username;
+    private String email;  
+    private String phone;  
+    private String password;  
+    private UserRole role;
+}
