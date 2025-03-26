c1b845e24dc3326a06b6ad5196a5c31fdb8583e9
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-03-26 23:25:15 +0800

    Update RunrunBotsBackendApplication.java

--- runrun_bots_backend/src/main/java/com/test/runrunbots/RunrunBotsBackendApplication.java
+++ runrun_bots_backend/src/main/java/com/test/runrunbots/RunrunBotsBackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableFeignClients
+@EnableFeignClients  // 扫描项目中所有标记了 @FeignClient 的接口，并将它们注册为 Spring Bean
 @EntityScan(basePackages = "com.test.runrunbots.model")
 @EnableJpaRepositories(basePackages = "com.test.runrunbots.repository")
 @SpringBootApplication
