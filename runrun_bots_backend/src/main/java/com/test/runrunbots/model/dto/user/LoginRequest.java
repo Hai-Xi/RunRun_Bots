96ce03b478371b0d640faaf984ae684ab244f611
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-03-26 12:10:09 +0800

    Update LoginRequest.java

--- runrun_bots_backend/src/main/java/com/test/runrunbots/model/dto/user/LoginRequest.java
+++ runrun_bots_backend/src/main/java/com/test/runrunbots/model/dto/user/LoginRequest.java
@@ -1,9 +1,15 @@
 package com.test.runrunbots.model.dto.user;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data  
-public class LoginRequest {  
+public class LoginRequest {
+
+    @NotNull(message = "用户名不能为空")
+    @Size(min = 3, max = 20, message = "用户名长度必须在3到20个字符之间")
     private String username;
+
     private String password;  
 }  
