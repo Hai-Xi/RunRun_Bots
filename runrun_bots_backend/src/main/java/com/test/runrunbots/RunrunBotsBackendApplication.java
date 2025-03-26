111ce5a6b70d9fb82d3058ae921ff77cd5c63042
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-03-26 22:43:58 +0800

    update map-api

--- runrun_bots_backend/src/main/java/com/test/runrunbots/RunrunBotsBackendApplication.java
+++ runrun_bots_backend/src/main/java/com/test/runrunbots/RunrunBotsBackendApplication.java
@@ -3,8 +3,10 @@ package com.test.runrunbots;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableFeignClients
 @EntityScan(basePackages = "com.test.runrunbots.model")
 @EnableJpaRepositories(basePackages = "com.test.runrunbots.repository")
 @SpringBootApplication
