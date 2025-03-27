69f39deed73b78b11319eed4cb5615527ff497bf
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-03-27 21:54:29 +0800

    Update RouteRequest.java

--- runrun_bots_backend/src/main/java/com/test/runrunbots/external/model/RouteRequest.java
+++ runrun_bots_backend/src/main/java/com/test/runrunbots/external/model/RouteRequest.java
@@ -14,6 +14,38 @@ import lombok.Data;
  * - 简洁：可以通过链式调用直接构造复杂嵌套对象, 不再需要 创建多个中间对象 逐步调用 set()。
  * - 可读：链式调用构造每个子对象，Builder 模式让代码更易读，所有参数在一个流中完整展现。使用嵌套 @Builder 构造复杂的层级数据更加直观。
  * - 安全：减少了对象在后续修改中丢失字段的风险，创建即为不可变对象。
+ * - 测试友好：( @Builder RouteResponse.java ) 更易于在单元测试中创建模拟数据。
+ *
+ * 测试和构建更简单
+ * 使用 Builder ，这种之前需要很多行代码构建请求对象的复杂代码：
+ *
+ * java
+ * // 先创建内层对象，再创建外层对象，层层嵌套
+ * RouteRequest.LatLng originLatLng = new RouteRequest.LatLng();
+ * originLatLng.setLatitude(37.419734);
+ * originLatLng.setLongitude(-122.0827784);
+ *
+ * RouteRequest.Location originLocation = new RouteRequest.Location();
+ * originLocation.setLatLng(originLatLng);
+ *
+ * RouteRequest.LocationData origin = new RouteRequest.LocationData();
+ * origin.setLocation(originLocation);
+ *
+ * // ... 还有很多代码
+ * 变成了更简洁、更可读的链式调用：
+ *
+ * java
+ * RouteRequest request = RouteRequest.builder()
+ *     .origin(RouteRequest.LocationData.builder()
+ *         .location(RouteRequest.Location.builder()
+ *             .latLng(RouteRequest.LatLng.builder()
+ *                 .latitude(37.419734)
+ *                 .longitude(-122.0827784)
+ *                 .build())
+ *             .build())
+ *         .build())
+ *     // ... 其他属性
+ *     .build();
  *
  *
  */
