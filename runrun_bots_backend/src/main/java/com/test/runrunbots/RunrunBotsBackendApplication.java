7ed6cb4b690ea00ffcc16400f9b73d3e53eaef8e
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-03-31 19:50:28 +0800

    feat(Add scheduled task to periodically update orderStatus):

--- runrun_bots_backend/src/main/java/com/test/runrunbots/RunrunBotsBackendApplication.java
+++ runrun_bots_backend/src/main/java/com/test/runrunbots/RunrunBotsBackendApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients  // 扫描项目中所有标记了 @FeignClient 的接口，并将它们注册为 Spring Bean
 @EntityScan(basePackages = "com.test.runrunbots.model")
 @EnableJpaRepositories(basePackages = "com.test.runrunbots.repository")
 @SpringBootApplication
+@EnableScheduling // Enables scheduled tasks
 public class RunrunBotsBackendApplication {
 
 	public static void main(String[] args) {
