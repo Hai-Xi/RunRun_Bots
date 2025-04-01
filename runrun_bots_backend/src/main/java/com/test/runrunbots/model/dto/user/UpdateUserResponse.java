68d6e65786169bde0809b865060d5cf7bce6cb06
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-04-01 21:46:57 +0800

    Update UpdateUserResponse.java

--- runrun_bots_backend/src/main/java/com/test/runrunbots/model/dto/user/UpdateUserResponse.java
+++ runrun_bots_backend/src/main/java/com/test/runrunbots/model/dto/user/UpdateUserResponse.java
@@ -4,6 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ *
+ * DTO（数据传输对象） 限定前端收到的字段
+ *
+ * 返回值最好使用自定义的 UpdateUserResponse dto 对象，而不是直接返回用户完整的实体类 User;
+ *
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
