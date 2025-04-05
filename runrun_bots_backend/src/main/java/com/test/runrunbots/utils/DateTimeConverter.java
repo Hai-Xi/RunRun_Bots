2a3aedc56ecef5d51022eb29c3d33cd5e7105f68
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-04-05 18:51:22 +0800

    Update DateTimeConverter.java

--- runrun_bots_backend/src/main/java/com/test/runrunbots/utils/DateTimeConverter.java
+++ runrun_bots_backend/src/main/java/com/test/runrunbots/utils/DateTimeConverter.java
@@ -2,8 +2,26 @@ package com.test.runrunbots.utils;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;  
-import java.util.Date;  
+import java.util.Date;
 
+/**
+ *
+ * 使用了系统默认时区：ZoneId.systemDefault()。
+ * 如果需要指定别的时区，可以修改方法中的 ZoneId，以满足不同地区的需求。例如：
+ * ```java
+ * ZoneId specificZoneId = ZoneId.of("UTC");
+ * ```
+ *
+ * | 特性 | Date | LocalDateTime |
+ * |------|------|---------------|
+ * | **可变性** | 可变（非线程安全） | 不可变（线程安全） |
+ * | **时区信息** | 隐式使用系统默认时区 | 不包含时区信息 |
+ * | **精度** | 毫秒 | 纳秒 |
+ * | **支持的操作** | 有限 | 丰富（加减日期时间、格式化等） |
+ * | **线程安全** | 否 | 是 |
+ * | **API 易用性** | 较差 | 优秀 |
+ *
+ */
 public class DateTimeConverter {  
 
     /**  
